833c4727bd4fdff187c8820242ad21938efd1817
JustinBenoist <justinbenoist76@gmail.com>
2026-02-18 12:15:57 -0500

    Adding back other pages

--- vite-config.js
+++ vite-config.js
@@ -2,3 +2,19 @@
 export default {
   base: '/'
 };
+
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        publications: resolve(__dirname, 'publications.html'),
+        nppm: resolve(__dirname, 'publications/nppm.html'),
+      }
+    }
+  }
+});
