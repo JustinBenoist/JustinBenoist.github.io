01ab001c1021c6d20684e62c9a8d88476469b534
JustinBenoist <justinbenoist76@gmail.com>
2026-03-03 15:14:50 -0500

    Update vite config for google verif

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        google6c1aaaeed7382f6e: resolve(__dirname, 'google6c1aaaeed7382f6e.html'),
         publications: resolve(__dirname, 'publications.html'),
         nppm: resolve(__dirname, 'public/publications/nppm.html'),
       }
